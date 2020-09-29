4b18bdf0cc5818d85943bb3d27c09af87028b41f
chromohh <sindre.he@hotmail.com>
2020-09-29 12:16:11 +0200

    Scoreboard init

---
+++ Javascript/React/react-assignment/src/score_display.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+/*
+ScoreCards for to display current score and information about past rounds
+Takes array, with 0 and 1 as values.
+0 = Lose
+1 = Win
+*/
+export default class ScoreCard extends React.Component{
+
+}
