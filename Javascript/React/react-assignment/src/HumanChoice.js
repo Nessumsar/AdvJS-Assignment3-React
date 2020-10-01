686056f5767fa24866d5b0ce0f992f685f22cb12
chromohh <sindre.he@hotmail.com>
2020-10-01 15:21:02 +0200

    humanchoice.js edits

--- Javascript/React/react-assignment/src/HumanChoice.js
+++ Javascript/React/react-assignment/src/HumanChoice.js
@@ -1,4 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-//Logik och presentation för spelarens val
+const options = [
+    {
+      emoji: '✊',
+      name: 'rock',
+      id: 1
+    },
+    {
+      emoji: '✋',
+      name: 'paper',
+      id: 2,
+    },
+    {
+      emoji: '✌',
+      name: 'scissor',
+      id: 3
+    }
+];
+
+export default function GenerateButtons({optionParentCallback}){
+  
+  const [option, setOption] = useState(0);
+
+  function chooseOption(option) {
+    setOption(option.name);
+    // optionParentCallback(option.name); // callback to parent hook
+  }
+
+  return(
+    <div>
+        <div>Current: {option}</div>
+        {options.map(option => (
+          <div key={option.name}>
+            <div className="option">
+              <span role="img" aria-label={option.name}>{option.emoji}</span>
+            </div>
+            <button onClick={() => chooseOption(option)}>pick</button>
+          </div>
+        ))}
+    </div>)
+}
