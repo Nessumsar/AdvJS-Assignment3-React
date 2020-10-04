466006258b2861fb702e56dfe2234d539baa69ca
Lukas <lukasrasmusseen@gmail.com>
2020-10-04 21:39:35 +0200

    design updates

--- Javascript/React/react-assignment/src/App.js
+++ Javascript/React/react-assignment/src/App.js
@@ -23,12 +23,17 @@ function App() {
     setAiChoice(updatedAiChoice);
   }, []); */
 
+  function logString(){
+    return "1-0"
+  }
+
   return (
     <div className="App">
         <Top getRounds={getRounds}/>
+        <p>Omgång 0 / {Rounds}</p>
         <HumanChoice getPlayerChoice={getPlayerChoice}/>
         <AiChoice/>
-        <Winner/>
+        <Winner logString={logString()}/>
     </div>
   );
 }
--- Javascript/React/react-assignment/src/Winner.js
+++ Javascript/React/react-assignment/src/Winner.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { render } from 'react-dom';
 import './css/Winner.css';
 
 
 //Räknar antal omgångar och poäng
 
 const Winner = (props) => {
-    
+    const [logList, setLogList] = useState([]);
+    const items = [];
 
-    return(<p>Winner-component</p>)
+    function updateList(){
+        logList.push(props.logString);
+        items.push(props.logString)
+    }
+
+    function renderList(){   
+        return(
+        <ul>
+            {items.map((item, index) => {
+                return <li key={index}>{item}</li>
+            })}
+        </ul>
+        );
+    }
+
+    return(
+    <div onLoad={updateList(props)}>
+        <div>
+            {renderList(props)}
+        </div>
+    </div>
+    );
 }
 
 
